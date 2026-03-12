45a14d4347f6a245b4cf513cc07b2334545848ac
wino444 <146556853+wino444@users.noreply.github.com>
2026-03-12 12:58:23 +0700

    🔑 อัพเดท key connect - วันที่ 12 เดือน 3 ปี 2026 เวลา 12:58 น.

--- key connect.lua
+++ key connect.lua
@@ -1,4 +1,4 @@
 return {
-    key = "wss://7567-2001-44c8-602f-ebfe-1c25-16ff-fe4f-77c9.ngrok-free.app",
-    updated = "วันที่ 12 เดือน 3 ปี 2026 เวลา 12:33 น."
+    key = "wss://5705-2001-44c8-602f-ebfe-1c25-16ff-fe4f-77c9.ngrok-free.app",
+    updated = "วันที่ 12 เดือน 3 ปี 2026 เวลา 12:58 น."
 }
